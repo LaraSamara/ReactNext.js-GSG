70b3736b2410736a364bd56ca77ad9423c1b54a5
LaraSamara <larasamara2002@gmail.com>
2024-11-15 19:30:35 +0200

    Task 4 and Task 5

--- assignment/product.ts
+++ assignment/product.ts
@@ -1,3 +1,22 @@
 // Task...4
+interface Product{
+    name: string;
+    price: number;
+}
+function sumPrice(products: Product[]): number{
+    return products.reduce((result, product) => result+product.price, 0);
+}
+const products: Product[] = [
+    {name: "Cap", price:150},
+    {name: "T-shirt", price:30},
+    {name: "Dress", price:250},
+    {name: "Skirt", price:99.9},
+]
+console.log(sumPrice(products));
 
 // Task...5
+function validEmail(email: string): boolean{
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+console.log(validEmail("larasamara@gmail.com"));
